0032220e067d7d94c241c4eb065317f5c7056c80
Marco Marchitto <marco.marchitto@iit.it>
2022-10-19 16:33:31 +0200

    Debug.

--- config/config.hpp
+++ config/config.hpp
@@ -54,10 +54,10 @@ const bool ACQUIRE_INITIAL_HEIGHT_MAP(false);
 const bool SET_DIAGONAL_MOVEMENT(false);
 
 // Footstep planning horizon
-const unsigned int FOOTSTEP_HORIZON(2);
+const unsigned int FOOTSTEP_HORIZON(3);
 
 // Minimum distance for cell to be considered traversable
-const float MIN_STAIR_DISTANCE(0.025);
+const float MIN_STAIR_DISTANCE(0.04);
 
 // Angle tolerance between target and robot rotation
 const float ANGLE_DIFFERENCE_TOLERANCE(2);
